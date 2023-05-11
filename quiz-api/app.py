12bed440f71feff3fe519833d199fb32dad90dae
barsollh <hugo.barsolle@edu.esiee.fr>
2023-05-11 11:06:30 +0200

    prout

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+from jwt_utils import *
 
 app = Flask(__name__)
 CORS(app)
@@ -8,10 +9,32 @@ CORS(app)
 def hello_world():
 	x = 'world'
 	return f"Hello, {x}"
-
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+@app.route('/login', methods=['POST'])
+def PostLogin():
+	payload = request.get_json()
+	if payload["password"] == "flask2023":
+		token = build_token()
+		return {"token": token}, 200
+	else:
+		return 'Unauthorized', 401
+	
+@app.route('/questions', methods=['POST'])
+def PostQuestion():
+	token = request.headers.get('Authorization')
+	try:
+		decode_token(token)
+	except:
+		return 'Unauthorized', 401
+	paylod = request.get_json()
+	return 200
+
+     
+#	return 'Unauthorized', 401
+
 
 if __name__ == "__main__":
     app.run()
---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Home page</h1>
+</template>
+
+<script>
+import quizApiService from "@/services/QuizApiService";
+
+export default {
+  name: "HomePage",
+  registeredScores: [],
+  data() {
+    return {
+      registeredScores
+    };
+  },
+  async created() {
+    console.log("Composant Home page 'created'");
+  }
+};
+</script>
